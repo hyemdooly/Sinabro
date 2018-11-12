a01beccb8063da767f84b9cbabcdf541df66354a
hyemdooly <hyemdooly@gmail.com>
2018-11-12 20:19:30 +0900

    back up

---
+++ app/src/main/java/me/android/hyemdooly/sinabro/Activity/MainActivity.kt
@@ -0,0 +1,33 @@
+package me.android.hyemdooly.sinabro.Activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.android.hyemdooly.sinabro.R
+
+class MainActivity : AppCompatActivity() {
+
+//    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+//        when (item.itemId) {
+//            R.id.translation -> {
+//                message.setText(R.string.title_translation)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.navigation_dashboard -> {
+//                message.setText(R.string.title_dashboard)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.navigation_notifications -> {
+//                message.setText(R.string.title_notifications)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//        }
+//        false
+//    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
